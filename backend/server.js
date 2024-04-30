509b47dd4eb0519dbeb240d96503556cfd10e2fa
Jatin Kumar <155115799+jatin800@users.noreply.github.com>
2024-05-01 02:53:44 +0530

    implement cors

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import router from "./routes/userRoutes.js";
 import { connectToMongoDB } from "./db/DB.js";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 connectToMongoDB();
 
