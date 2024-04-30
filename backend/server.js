80bf714e149d2ca163720a3031cfb9fc6dc9454e
Jatin Kumar <155115799+jatin800@users.noreply.github.com>
2024-04-30 23:12:28 +0530

    setup MONGODB,ROUTES,CONTROLLERS

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,28 @@
 import express from "express";
+import mongoose from "mongoose";
+import User from "./model/userModel.js";
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello");
+mongoose
+  .connect("mongodb://localhost:27017/mernstack_crud")
+  .then(() => console.log("Connect to MongoDB"))
+  .catch((error) => console.log(" Error Connecting to MongoDB", error));
+
+app.post("/createuser", async (req, res) => {
+  try {
+    const dataBody = req.body;
+    const user = new User(dataBody);
+    const userCreated = await user.save();
+    res.send(userCreated);
+  } catch (error) {
+    console.log("Error in CreateUser", error);
+    res.status(500).send(error);
+  }
 });
 
 app.listen(PORT),
